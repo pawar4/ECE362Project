47abcd2ba1822736ee796754fa63c6165b649000
Akshay Pawar <42657031+pawar4@users.noreply.github.com>
2019-12-02 02:41:12 -0500

    Update helper.h

--- helper.h
+++ helper.h
@@ -1,16 +1,19 @@
 void init_keypad();
 void init_TIM2();
-int get_key_press();
+void get_key_press();
 void spi_cmd(char b);
 void spi_data(char b);
 void spi_init_lcd(void);
 void dma_spi_init_lcd(void);
 void circdma_display1(const char *s);
 void circdma_display2(const char *s);
-void init_RTC();
+void init_RTC(int hr, int min, int h12);
 void rtcGetTime(char* timeStr);
 void init_TIM14();
-void get_char(char key);
+void get_char();
+void initAlarm(int hr, int min, char h12);
+void inputAlarm();
+void inputTime();
 
 void (*cmd)(char b) = 0;
 void (*data)(char b) = 0;
@@ -21,6 +24,21 @@ int8_t lookup[16] = {1,4,7,0xe,2,5,8,0,3,6,9,0xf,0xa,0xb,0xc,0xd};
 char char_lookup[16] = {'1','4','7','*','2','5','8','0','3','6','9','#','A','B','C','D'};
 int timeofday = 9;
 char clockTime[16];
-int row = 1;
+int col = 1;
 char keys[16] = {"Key Pressed:"};
-//char key;
+char key;
+int value;
+int press = 0;
+
+#define RATE 100000
+#define N 1000
+short int wavetable[N];
+int freq = 1000;
+int freq2 = 1000;
+int i = 0;
+int j = 0;
+int offset = 0;
+int offset2 = 0;
+int step = 1000 * N / 100000.0 * (1 << 16);
+int step2 = 1000 * N / 100000.0 * (1 << 16);
+
